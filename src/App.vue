95e4362fb0246fd94bc9af414b5804318ff8d427
amalgamik <amalgamik@gmail.com>
2021-03-03 18:42:02 +0200

    FIRST

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <loginPage msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import loginPage from './components/loginPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    loginPage,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/loginPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <form>
+    <div class="row mb-3">
+      <label for="inputEmail3" class="col-sm-2 col-form-label">Login</label>
+      <div class="col-sm-4">
+        <input type="email" class="form-control" id="inputEmail3">
+      </div>
+    </div>
+    <div class="row mb-3">
+      <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+      <div class="col-sm-4">
+        <input type="password" class="form-control" id="inputPassword3">
+      </div>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Войти</button>
+  </form>
+
+</template>
+
+<script>
+export default {
+  name: 'loginPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
